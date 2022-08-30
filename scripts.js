37b270839bb1dfa28954f5997cc463a49ef881b8
Tor <tor.devries@wsu.edu>
2022-08-30 22:16:29 +0000

    added the recipe object and new recipe for tiramisu

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// JavaScript for TP6
+// JavaScript for TP7
 
 // function to load a file from the URL "fromFile" into the object identified by "whereTo"
 function loadFileInto(fromFile, whereTo) {
@@ -12,7 +12,7 @@ function loadFileInto(fromFile, whereTo) {
 	// provides code to do something in response to the AJAX request
 	ajax.onreadystatechange = function() {
 			if ((this.readyState == 4) && (this.status == 200)) {
-				document.querySelector(whereTo).innerHTML += this.responseText;
+				document.querySelector(whereTo).innerHTML = this.responseText;
 				
 			} else if ((this.readyState == 4) && (this.status != 200)) {
 				console.log("Error: " + this.responseText);
@@ -27,11 +27,59 @@ function loadFileInto(fromFile, whereTo) {
 
 
 
+// new Recipe object
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+	
+	this.recipeName = recipeName;
+	this.contributor = contributorName;
+	this.imageURL = imageURL;
+	this.ingredients = ingredientsURL;
+	this.equipment = equipmentURL;
+	this.directions = directionsURL;
+	
+	this.displayRecipe = function() {
+		
+		document.querySelector("#titleBanner h1").innerHTML = this.recipeName;
+		document.querySelector("#contributor").innerHTML = this.contributor;
+		document.querySelector("#titleBanner").style.backgroundImage = "url(" + this.imageURL + ")";
+		loadFileInto(this.ingredients, "#ingredients ul");
+		loadFileInto(this.equipment, "#equipment ul");
+		loadFileInto(this.directions, "#directions ol");
+		
+	}
+	
+}
+
+
+SevenLayerBars = new Recipe(
+	"Seven Layer Bars", 
+	"Tor", 
+	"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F5574746.jpg&w=596&h=596&c=sc&poi=face&q=85", 
+	"ingredients.html", 
+	"equipment.html", 
+	"directions.html"
+);
+
+Tiramisu = new Recipe(
+	"Tiramisu", 
+	"Alison Roman", 
+	"https://static01.nyt.com/images/2017/04/05/dining/05COOKING-TIRAMISU1/05COOKING-TIRAMISU1-master768.jpg?w=1280&q=75", 
+	"tiramisu-ingredients.html", 
+	"tiramisu-equipment.html", 
+	"tiramisu-directions.html"
+);
+
 
 
 window.onload = function() {
 	
+	document.querySelector("#firstRecipe").onclick = function() {
+		SevenLayerBars.displayRecipe();
+	}
 
+	document.querySelector("#secondRecipe").onclick = function() {
+		Tiramisu.displayRecipe();
+	}
 	
 	
 } // end window.onload
